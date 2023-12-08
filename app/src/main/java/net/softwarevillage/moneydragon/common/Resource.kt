938eada6100f489cb6ea99c5572941d5bb69ba44
Bashirli <bashirli1903@gmail.com>
2023-12-08 18:30:03 +0400

    test commit

--- app/src/main/java/net/softwarevillage/moneydragon/common/Resource.kt
+++ app/src/main/java/net/softwarevillage/moneydragon/common/Resource.kt
@@ -4,4 +4,5 @@ sealed class Resource<out T> {
     object Loading : Resource<Nothing>()
     data class Success<out T>(val result: T?) : Resource<T>()
     data class Error(val throwable: Exception) : Resource<Nothing>()
+
 }
